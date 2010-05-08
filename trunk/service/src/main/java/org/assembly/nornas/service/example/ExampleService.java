d3e0b2c635e8edf28c0c5f6bb31b38cf7ce5af93
emanuelpeg <emanuelpeg@c4c82b18-dfe9-b6a7-31fb-6a3d1f398010>
2010-05-08 16:25:59 +0000

    Desarrollo de ejemplo

---
+++ trunk/service/src/main/java/org/assembly/nornas/service/example/ExampleService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.assembly.nornas.service.example;
+
+import org.osoa.sca.annotations.Remotable;
+
+/**
+ * @author emanuel
+ * 
+ *         Sample Service
+ * 
+ */
+@Remotable
+public interface ExampleService {
+
+	String sayHello();
+
+}
